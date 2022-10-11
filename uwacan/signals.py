7af18fb371e538ba3f0feb24d606a1e19efe112e
Carl Andersson <carl.andersson@ivl.se>
2022-10-11 16:10:47 +0200

    Separates behaviors in signals
    There's now different classes to handle time and frequency properties.

--- uwacan/signals.py
+++ uwacan/signals.py
@@ -4,19 +4,83 @@ from . import positional
 
 
 class Signal:
-    def __init__(self, data, samplerate, start_time, downsampling=None):
+    axes = tuple()
+    def __init__(self, data, axes=None):
         self.data = np.asarray(data)
+        if axes is not None:
+            self.axes = axes
+        if len(self.axes) != self.data.ndim:
+            if len(self.axes) < self.data.ndim:
+                self.axes = self.axes[-self.data.ndim]
+            else:
+                raise ValueError('The number of dimensions in the data does not match the number of expected axes')
+        # if self.data.ndim == len(self.axes) + 1:
+            # Override the class default with a `channels` axis?
+            # self.axes = ('channels', ) + self.axes
+
+    def copy(self, deep=False, _new_class=None):
+        if _new_class is None:
+            _new_class = type(self)
+        obj = _new_class.__new__(_new_class)
+        if deep:
+            obj.data = self.data.copy()
+        else:
+            obj.data = self.data
+        obj.axes = self.axes
+        return obj
+
+    def apply(self, func, *args, axis=None, **kwargs):
+        if axis is None:
+            obj = self.copy()
+            obj.data = func(obj.data, *args, **kwargs)
+            obj.axes = self.axes[len(self.axes) - np.ndim(obj.data):]
+            return obj
+
+        if isinstance(axis, (int, str)):
+            axis = [axis]
+        reduce_axes = []
+        for ax in axis:
+            if isinstance(ax, str):
+                ax = self.axes.index(ax)
+            reduce_axes.append(ax)
+        reduce_axes = tuple(reduce_axes)
+        new_axes = tuple(ax for idx, ax in enumerate(self.axes) if idx not in reduce_axes)
+
+        obj = self.copy()
+        obj.data = func(obj.data, *args, axis=reduce_axes, **kwargs)
+        obj.axes = new_axes
+        return obj
+
+    def sum(self, axis=None):
+        return self.apply(np.sum, axis=axis)
+
+    def mean(self, axis=None):
+        return self.apply(np.nanmean, axis=axis)
+
+    @classmethod
+    def stack(cls, items, axis):
+        obj = items[0].copy(_new_class=cls)
+        obj.data = np.stack([item.data for item in items], axis=0)
+        obj.axes = (axis,) + obj.axes
+        return obj
+
+
+class Time(Signal):
+    axes = ('channels', 'time')
+
+    def __init__(self, data, samplerate, start_time, downsampling=None, **kwargs):
+        super().__init__(data=data, **kwargs)
+        # self.data = np.asarray(data)
         self.samplerate = samplerate
         self.downsampling = downsampling
+        # TODO: parse the start time here as well, if it's a string.
         self._start_time = start_time
 
-
-    def copy(self):
-        obj = type(self).__new__(type(self))
+    def copy(self, **kwargs):
+        obj = super().copy(**kwargs)
         obj.samplerate = self.samplerate
         obj.downsampling = self.downsampling
-        obj.time_window = self.time_window
-        obj.data = self.data
+        obj._start_time = self._start_time
         return obj
 
     @property
@@ -26,6 +90,14 @@ class Signal:
     @property
     def datarate(self):
         return self.samplerate if self.downsampling is None else self.samplerate / self.downsampling
+
+    @property
+    def relative_time(self):
+        return np.arange(self.num_samples) / self.datarate
+
+    @property
+    def timestamps(self):
+        return [self._start_time + positional.datetime.timedelta(seconds=t) for t in self.relative_time]
 
     @property
     def time_window(self):
@@ -41,11 +113,13 @@ class Signal:
         if isinstance(key, slice):
             start, stop = key.start, key.stop
             if isinstance(start, positional.datetime.datetime):
-                start = (start - self._start).total_seconds()
-                start = np.math.ceil(start * self.datarate)
+                start = (start - self._start_time).total_seconds()
             if isinstance(stop, positional.datetime.datetime):
-                stop = (stop - self._start).total_seconds()
-                stop = np.math.floor(stop * self.datarate)
+                stop = (stop - self._start_time).total_seconds()
+
+            # Indices assumed to be seconds from start
+            start = np.math.floor(start * self.datarate)
+            stop = np.math.ceil(stop * self.datarate)
 
             obj = self.copy()
             obj.data = self.data[..., start:stop]
@@ -55,7 +129,7 @@ class Signal:
         raise IndexError('only TimeWindows or slices of integers/datetimes are valid indices to Signal containers')
 
 
-class Pressure(Signal):
+class Pressure(Time):
     @classmethod
     def from_raw_and_calibration(cls, raw_signal, calibration, **kwargs):
         """Create a pressure signal from raw values and known calibration.
@@ -73,16 +147,36 @@ class Pressure(Signal):
             If `raw_signal` is in "file units", e.g. scaled to [-1, 1] fullscale,
             the calibration value must scale from "file units" to μPa.
         """
+        # raw_signal = np.atleast_2d(raw_signal)
         raw_signal = np.asarray(raw_signal)
-        calibration = np.asarray(calibration)
-        c = 10**(calibration / 20) / 1e-6  # Calibration values are given as dB re.
-        calibrated = raw_signal / c.reshape((-1,) + (1,) * (raw_signal.ndim - 1))
+        calibration = np.asarray(calibration).astype('float32')
+        c = 10**(calibration / 20) / 1e-6  # Calibration values are given as dB re. 1μPa
+        c = c.reshape((-1,) + (1,) * (raw_signal.ndim - 1))
+        if raw_signal.dtype in (np.int8, np.int16, np.int32, np.float32):
+            c = c.astype(np.float32)
+
+        calibrated = raw_signal / c
         obj = cls(data=calibrated, **kwargs)
         return obj
 
 
-class Spectrogram(Signal):
-    # TODO: Shapes! This gets data.shape (n_ch, n_freq, n_time)
+class Frequency(Signal):
+    axes = ('channels', 'frequency')
+
+    def __init__(self, data, frequency, bandwidth, **kwargs):
+        super().__init__(data=data, **kwargs)
+        self.frequency = frequency
+        self.bandwidth = bandwidth
+
+    def copy(self, **kwargs):
+        obj = super().copy(**kwargs)
+        obj.frequency = self.frequency
+        obj.bandwidth = self.bandwidth
+        return obj
+
+
+class Spectrogram(Time, Frequency):
+    axes = ('channels', 'frequency', 'time')
     def __init__(self, time_signal, window_duration=None, window='hann', overlap=0.5):
         window_samples = round(window_duration * time_signal.samplerate)
         overlap_samples = round(window_duration * overlap * time_signal.samplerate)
@@ -94,55 +188,31 @@ class Spectrogram(Signal):
             noverlap=overlap_samples,
         )
         super().__init__(
-            data=Sxx,
+            data=Sxx.copy(),  # Using a copy here is a performance improvement in later processing stages.
+            # The array returned from the spectrogram function is the real part of the original stft, reshaped.
+            # This means that the array takes twice the memory (the imaginary part is still around),
+            # and it's not contiguous which slows down filtering a lot.
             samplerate=time_signal.samplerate,
-            start_time=time_signal.time_window.start,
+            start_time=time_signal.time_window.start + positional.datetime.timedelta(seconds=t[0]),
             downsampling=window_samples - overlap_samples,
+            frequency=f,
+            bandwidth=time_signal.samplerate / window_samples,
         )
-        self.frequencies = f
-        self.time = t
-
-    def copy(self):
-        obj = super().copy()
-        obj.frequencies = self.frequencies
-        obj.time = self.time
 
 
-class NthOctavebandFilterBank:
-    # TODO: Shapes! This outputs signals with data.shape (n_ch, n_bands, n_time)
-    # TODO: Make a simple class to hold "power signals".
-    # It's probably a good idea to have some way to detect that we're working with bandpassed signals, and some way to detect that we're working with power signals?
-    def __init__(self, frequency_range, bands_per_octave=3, filter_order=8):
-        self.frequency_range = frequency_range
-        self.bands_per_octave = bands_per_octave
-        self.filter_order = filter_order
+class PowerBands(Frequency):
+    pass
 
-    def __call__(self, signal):  # TODO: Call signature!
-        if isinstance(signal, Spectrogram):
-            powers = self.power_filters(signal.frequencies).dot(signal.data) / signal.frequencies[1]
-            return Signal(
-                data=powers,
-                samplerate=signal.samplerate,
-                start_time=signal.time_window.start,
-                downsampling=signal.downsampling,
-            )
-        else:
-            raise TypeError(f'Cannot filter data of input type {type(signal)}')
 
-    @property
-    def center_frequencies(self):
-        lowest_band, highest_band = self.frequency_range
-        lowest_band_index = np.round(self.bands_per_octave * np.log2(lowest_band / 1e3))
-        highest_band_index = np.round(self.bands_per_octave * np.log2(highest_band / 1e3))
-        octaves = np.arange(lowest_band_index, highest_band_index + 1) / self.bands_per_octave
-        return 1e3 * 2 ** octaves
+class SourceSpectrum(Frequency):
+    axes = ('runs', 'segments', 'channels', 'frequency')
 
-    def power_filters(self, frequencies):
-        centers = self.center_frequencies[:, None]
-        bandwidths = centers * (2**(0.5 / self.bands_per_octave) - 2**(-0.5 / self.bands_per_octave))
-        filters = 1 / (
-            1
-            + ((frequencies**2 - centers**2) / (frequencies * bandwidths))
-            ** (2 * self.filter_order)
-        )
-        return filters
+
+class PowerBandSignal(Time, Frequency):
+    axes = ('channels', 'frequency', 'time')
+
+    def mean(self, *args, **kwargs):
+        obj = super().mean(*args, **kwargs)
+        if 'time' not in obj.axes:
+            obj = obj.copy(_new_class=PowerBands)
+        return obj

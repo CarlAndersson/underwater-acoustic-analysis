fd1ce81592869085e91222ce4a6796e37e19767b
Carl Andersson <carl.andersson@ivl.se>
2022-10-11 16:19:29 +0200

    Changes to TL call signatures
    This adds more info to the call signatures in the transmission loss
    models.

--- uwacan/transmission_loss.py
+++ uwacan/transmission_loss.py
@@ -1,4 +1,5 @@
 import numpy as np
+from . import signals
 
 # TODO: As the models become more advanced we have to include more inputs to the calculation.
 # Otherwise we have no way of knowing at what frequencies to evaluate the TL.
@@ -6,70 +7,82 @@ import numpy as np
 
 
 class TransmissionModel:
-    def pressure_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return 1 / self.pressure_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time)
+    def compensate(self, signal, receiver, source_track, time=None):
+        if isinstance(signal, signals.PowerBands):
+            compensation = self.power_loss(receiver=receiver, source_track=source_track, frequency=signal.frequency, time=time)
+            obj = signal.copy(_new_class=signals.SourceSpectrum)
+            obj.data = obj.data * compensation
+        else:
+            raise TypeError(f'Cannot compensate transmission to {signal.__class__.__name__}')
+        return obj
 
-    def pressure_loss(self, receiver, source_track, frequencies=None, time=None):
-        return 1 / self.pressure_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time)
+    def pressure_transfer(self, receiver, source_track, frequency=None, time=None):
+        return 1 / self.pressure_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time)
 
-    def power_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return 1 / self.power_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time)
+    def pressure_loss(self, receiver, source_track, frequency=None, time=None):
+        return 1 / self.pressure_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time)
 
-    def power_loss(self, receiver, source_track, frequencies=None, time=None):
-        return 1 / self.power_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time)
+    def power_transfer(self, receiver, source_track, frequency=None, time=None):
+        return 1 / self.power_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time)
 
-    def level_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return -self.level_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time)
+    def power_loss(self, receiver, source_track, frequency=None, time=None):
+        return 1 / self.power_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time)
 
-    def level_loss(self, receiver, source_track, frequencies=None, time=None):
-        return -self.level_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time)
+    def level_transfer(self, receiver, source_track, frequency=None, time=None):
+        return -self.level_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time)
+
+    def level_loss(self, receiver, source_track, frequency=None, time=None):
+        return -self.level_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time)
 
 
 class PressureModel(TransmissionModel):
-    def power_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return self.pressure_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time) ** 2
+    def power_transfer(self, receiver, source_track, frequency=None, time=None):
+        return self.pressure_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time) ** 2
 
-    def power_loss(self, receiver, source_track, frequencies=None, time=None):
-        return self.pressure_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time) ** 2
+    def power_loss(self, receiver, source_track, frequency=None, time=None):
+        return self.pressure_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time) ** 2
 
-    def level_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return 20 * np.log10(self.pressure_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time))
+    def level_transfer(self, receiver, source_track, frequency=None, time=None):
+        return 20 * np.log10(self.pressure_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time))
 
-    def level_loss(self, receiver, source_track, frequencies=None, time=None):
-        return 20 * np.log10(self.pressure_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time))
+    def level_loss(self, receiver, source_track, frequency=None, time=None):
+        return 20 * np.log10(self.pressure_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time))
 
 
 class PowerModel(TransmissionModel):
-    def pressure_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return self.power_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time) ** 0.5
+    def pressure_transfer(self, receiver, source_track, frequency=None, time=None):
+        return self.power_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time) ** 0.5
 
-    def pressure_loss(self, receiver, source_track, frequencies=None, time=None):
-        return self.power_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time) ** 0.5
+    def pressure_loss(self, receiver, source_track, frequency=None, time=None):
+        return self.power_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time) ** 0.5
 
-    def level_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return 10 * np.log10(self.power_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time))
+    def level_transfer(self, receiver, source_track, frequency=None, time=None):
+        return 10 * np.log10(self.power_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time))
 
-    def level_loss(self, receiver, source_track, frequencies=None, time=None):
-        return 10 * np.log10(self.power_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time))
+    def level_loss(self, receiver, source_track, frequency=None, time=None):
+        return 10 * np.log10(self.power_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time))
 
 
 class LevelModel(TransmissionModel):
-    def pressure_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return 10 ** (self.level_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time) / 20)
+    def pressure_transfer(self, receiver, source_track, frequency=None, time=None):
+        return 10 ** (self.level_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time) / 20)
 
-    def pressure_loss(self, receiver, source_track, frequencies=None, time=None):
-        return 10 ** (self.level_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time) / 20)
+    def pressure_loss(self, receiver, source_track, frequency=None, time=None):
+        return 10 ** (self.level_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time) / 20)
 
-    def power_transfer(self, receiver, source_track, frequencies=None, time=None):
-        return 10 ** (self.level_transfer(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time))
+    def power_transfer(self, receiver, source_track, frequency=None, time=None):
+        return 10 ** (self.level_transfer(receiver=receiver, source_track=source_track, frequency=frequency, time=time) / 10)
 
-    def power_loss(self, receiver, source_track, frequencies=None, time=None):
-        return 10 ** (self.level_loss(receiver=receiver, source_track=source_track, frequencies=frequencies, time=time))
+    def power_loss(self, receiver, source_track, frequency=None, time=None):
+        return 10 ** (self.level_loss(receiver=receiver, source_track=source_track, frequency=frequency, time=time) / 10)
 
 
 class MlogR(LevelModel):
     def __init__(self, M=20):
         self.M = M
 
-    def level_loss(self, receiver, source_track, frequencies=None, time=None):
+    def level_loss(self, receiver, source_track, frequency=None, time=None):
         return self.M * np.log10(source_track.distance_to(receiver.position))
+
+
+# TODO: We need a way to create a TL model from measurements.

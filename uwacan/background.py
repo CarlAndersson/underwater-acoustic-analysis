e979d0c9411fd5d3d4663167783224e042d84480
Carl Andersson <carl.andersson@ivl.se>
2023-07-13 17:29:19 +0200

    Fixes to background measurement
    This is to align with the uptades in other places

--- uwacan/background.py
+++ uwacan/background.py
@@ -1,19 +1,18 @@
-from . import analysis
+from . import analysis, positional
 import xarray as xr
 import numpy as np
 
 
 class MeasuredDecidecadeBackgroundCompensation:
-    def __init__(self, segments, frequency_range=(10, 50000), snr_requirement=3):
-        filterbank = analysis.DecidecadeFilterbank(frequency_range=frequency_range)
+    def __init__(self, segments, filterbank=None, snr_requirement=3):
+        if filterbank is None:
+            filterbank = analysis.DecidecadeFilterbank(window_duration=1, lower_bound=10, upper_bound=50_000)
         self.snr_requirement = snr_requirement
 
         segment_powers = []
         for segment in segments:
-            signal = segment.time_data
-            spectrogram = analysis.spectrogram(signal, 1)
-            power = filterbank(spectrogram)
-            power = power.mean(dim='time').assign_coords(segment=segment.sampling.window.center)
+            power = filterbank(segment.time_data)
+            power = power.mean(dim='time').assign_coords(segment=positional._datetime_to_np(segment.sampling.window.center))
             segment_powers.append(power)
         self.segment_powers = xr.concat(segment_powers, dim='segment')
         self.average_power = self.segment_powers.pipe(np.log).mean(dim='segment').pipe(np.exp)

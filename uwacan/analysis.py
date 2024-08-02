ccd3f9981d5a343a412eadf7bdef5b38287fb3a9
Carl Andersson <carl.andersson@ivl.se>
2024-08-02 10:15:21 +0200

    Implements wrapper for NthDecadeSpectrogram
    The core implementation is just modified from the previous implementation.

--- uwacan/analysis.py
+++ uwacan/analysis.py
@@ -400,6 +400,203 @@ class Spectrogram(TimeFrequencyData):
         obj.frame_settings = self.frame_settings
 
 
+class NthDecadeSpectrogram(TimeFrequencyData):
+    """Calculates Nth-decade spectrograms.
+
+    The processing is done in stft frames determined by `frame_duration`, `frame_step`
+    `frame_overlap`, and `hybrid_resolution`. At least one of "duration", "step",
+    or "resolution" has to be given, see `time_frame_settings` for further details.
+    At least one of `lower_bound` and `hybrid_resolution` has to be given.
+    Note that the `frame_duration` and `frame_step` can be auto-chosen from the overlap
+    and required frequency resolution, either from `hybrid_resolution` or `lower_bound`.
+
+    Parameters
+    ----------
+    data : TimeData or Spectrogram, optional
+        The time data from which to calculate the spectrogram.
+        Omit this to create a callable object for later evaluation.
+    frame_duration : float
+        The duration of each stft frame, in seconds.
+    frame_step : float
+        The time step between stft frames, in seconds.
+    frame_overlap : float, default 0.5
+        The overlap factor between stft frames. A negative value leaves
+        gaps between frames.
+    lower_bound : float
+        The lowest frequency to include in the processing.
+    upper_bound : float
+        The highest frequency to include in the processing.
+    hybrid_resolution : float
+        A frequency resolution to aim for. Only used if `frame_duration` is not given.
+    scaling : str, default="density"
+        The scaling to use for the output.
+        - "density" scales the output as a power spectral density.
+        - "power" scales the output as the total power in each band.
+    fft_window : str, default="hann"
+        The shape of the window used for the stft.
+
+    Raises
+    ------
+    ValueError
+        If the processing settings are not compatible, e.g.,
+        - frequency bands with bandwidth smaller than the frame duration allows,
+        - no lower bound and no hybrid resolution.
+    """
+    def __init__(
+        self,
+        data=None,
+        bands_per_decade=None,
+        frame_step=None,
+        frame_duration=None,
+        frame_overlap=0.5,
+        lower_bound=None,
+        upper_bound=None,
+        hybrid_resolution=None,
+        scaling="density",
+        fft_window="hann",
+        **kwargs
+    ):
+        try:
+            if hybrid_resolution not in {True, False}:
+                resolution = hybrid_resolution
+            else:
+                resolution = lower_bound * (10 ** (0.5 / bands_per_decade) - 10 ** (-0.5 / bands_per_decade))
+            self.frame_settings = time_frame_settings(
+                duration=frame_duration,
+                step=frame_step,
+                resolution=resolution,
+                overlap=frame_overlap,
+            ) | {"window": fft_window}
+        except ValueError:
+            pass
+
+        self.bands_per_decade = bands_per_decade
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.scaling = scaling
+        self.hybrid_resolution = hybrid_resolution
+
+        if isinstance(data, TimeData):
+            # __call__ will create an object that we only use the .data from
+            data = self(data).data
+        if data is not None:
+            super().__init__(data, **kwargs)
+
+    def __call__(self, data):
+        if isinstance(data, type(self)):
+            return data
+        if not isinstance(data, Spectrogram):
+            data = Spectrogram(
+                data,
+                frame_step=self.frame_settings["step"],
+                frame_duration=self.frame_settings["duration"],
+                frame_overlap=self.frame_settings["overlap"],
+                frequency_resolution=self.frame_settings["resolution"],
+            )
+
+        if self.hybrid_resolution:
+            if self.hybrid_resolution is True:
+                hybrid_resolution = 1 / data.frame_settings["duration"]
+            else:
+                hybrid_resolution = self.hybrid_resolution
+            if hybrid_resolution * data.frame_settings["duration"] < 1:
+                raise ValueError(
+                    f'Hybrid filterbank with resolution of {hybrid_resolution:.2f} Hz '
+                    f'cannot be calculated from temporal windows of {data.frame_settings["duration"]:.2f} s.'
+                )
+        else:
+            hybrid_resolution = False
+            lowest_bandwidth = self.lower_bound * (10 ** (0.5 / self.bands_per_decade) - 10 ** (-0.5 / self.bands_per_decade))
+            if lowest_bandwidth * data.frame_settings["duration"] < 1:
+                raise ValueError(
+                    f'{self.bands_per_decade}th-decade filter band at {self.lower_bound:.2f} Hz with bandwidth of {lowest_bandwidth:.2f} Hz '
+                    f'cannot be calculated from temporal windows of {data.frame_settings["duration"]:.2f} s.'
+                )
+
+        spec_xr = data.data.transpose("frequency", ...)
+        # Get frequency centers for the new bands
+        bands_per_decade = self.bands_per_decade
+        log_band_scaling = 10 ** (0.5 / bands_per_decade)
+        lower_bound = self.lower_bound or 0  # Prevent None or False.
+        upper_bound = self.upper_bound or spec_xr.frequency.data[-1] / log_band_scaling
+        if hybrid_resolution:
+            # The frequency at which the logspaced bands cover at least one linspaced band
+            minimum_bandwidth_frequency = hybrid_resolution / (log_band_scaling - 1 / log_band_scaling)
+            first_log_idx = np.math.ceil(bands_per_decade * np.log10(minimum_bandwidth_frequency / 1e3))
+            last_linear_idx = np.math.floor(minimum_bandwidth_frequency / hybrid_resolution)
+
+            # Since the logspaced bands have pre-determined centers, we can't just start them after the linspaced bands.
+            # Often, the bands will overlap at the minimum bandwidth frequency, so we look for the first band
+            # that does not overlap, i.e., the upper edge of the last linspaced band is below the lower edge of the first
+            # logspaced band
+            while (last_linear_idx + 0.5) * hybrid_resolution > 1e3 * 10 ** ((first_log_idx - 0.5) / bands_per_decade):
+                # Condition is "upper edge of last linear band is higher than lower edge of first logarithmic band"
+                last_linear_idx += 1
+                first_log_idx += 1
+
+            if last_linear_idx * hybrid_resolution > upper_bound:
+                last_linear_idx = np.math.floor(upper_bound / hybrid_resolution)
+            first_linear_idx = np.math.ceil(lower_bound / hybrid_resolution)
+        else:
+            first_linear_idx = last_linear_idx = 0
+            first_log_idx = np.round(bands_per_decade * np.log10(lower_bound / 1e3))
+
+        last_log_idx = round(bands_per_decade * np.log10(upper_bound / 1e3))
+
+        lin_centers = np.arange(first_linear_idx, last_linear_idx) * hybrid_resolution
+        lin_lowers = lin_centers - 0.5 * hybrid_resolution
+        lin_uppers = lin_centers + 0.5 * hybrid_resolution
+
+        log_centers = 1e3 * 10 ** (np.arange(first_log_idx, last_log_idx + 1) / bands_per_decade)
+        log_lowers = log_centers / log_band_scaling
+        log_uppers = log_centers * log_band_scaling
+
+        centers = np.concatenate([lin_centers, log_centers])
+        lowers = np.concatenate([lin_lowers, log_lowers])
+        uppers = np.concatenate([lin_uppers, log_uppers])
+
+        # Aggregate input spectrum into bands
+        spec_np = spec_xr.data
+        banded_data = np.full(centers.shape + spec_np.shape[1:], np.nan)
+        spectral_resolution = 1 / data.frame_settings["duration"]
+        for idx, (l, u) in enumerate(zip(lowers, uppers)):
+            l_idx = int(np.floor(l / spectral_resolution + 0.5))  # (l_idx - 0.5) * Δf = l
+            u_idx = int(np.ceil(u / spectral_resolution - 0.5))  # (u_idx + 0.5) * Δf = u
+            l_idx = max(l_idx, 0)
+            u_idx = min(u_idx, spec_np.shape[0] - 1)
+
+            if l_idx == u_idx:
+                # This can only happen if both frequencies l and u are within the same fft bin.
+                # Since we don't allow the fft bins to be larger than the output bins, we thus have the exact same band.
+                banded_data[idx] = spec_np[l_idx]
+            else:
+                # weight edge bins by "(whole bin - what is not in the new band) / whole bin"
+                # lower fft bin edge l_e = (l_idx - 0.5) * Δf
+                # w_l = (Δf - (l - l_e)) / Δf = l_idx + 0.5 - l / Δf
+                first_weight = l_idx + 0.5 - l / spectral_resolution
+                # upper fft bin edge u_e = (u_idx + 0.5) * Δf
+                # w_u = (Δf - (u_e - u)) / Δf = 0.5 - u_idx + u / Δf
+                last_weight = u / spectral_resolution - u_idx + 0.5
+                # Sum the components fully within the output bin `[l_idx + 1:u_idx]`, and weighted components partially in the band.
+                this_band = (
+                    spec_np[l_idx + 1 : u_idx].sum(axis=0)
+                    + spec_np[l_idx] * first_weight
+                    + spec_np[u_idx] * last_weight
+                )
+                banded_data[idx] = this_band * (spectral_resolution / (u - l))  # Rescale the power density.
+        banded = type(self)(
+            data=banded_data,
+            start_time=spec_xr.time[0],
+            samplerate=spec_xr.time.rate,
+            frequency=centers,
+            bandwidth=uppers - lowers,
+            dims=spec_xr.dims,
+            coords=spec_xr.coords,
+        )
+        if not self.scaling == "density":
+            banded *= banded.bandwidth
+        self._transfer_attributes(banded)
+        return banded
 
 
 def bureau_veritas_source_spectrum(

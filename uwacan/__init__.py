10cec43a5c47e6ffbec3e5237b1351cf7e97b95d
Carl Andersson <carl.andersson@ivl.se>
2024-08-09 14:58:00 +0200

    Lift the most useful positional classes to the top namespace

--- uwacan/__init__.py
+++ uwacan/__init__.py
@@ -4,13 +4,27 @@ __version_info__ = _version.version_info
 __version__ = _version.version
 del _version
 
-from . import (
+from . import (  # noqa: E402
     positional,
     recordings,
     analysis,
     propagation,
     background,
+    source_models,
     visualization,
 )  # noqa: E402, F401
 
-from ._core import *
+from ._core import (
+    TimeWindow,
+    dB,
+    TimeData,
+    FrequencyData,
+    TimeFrequencyData,
+    Transit,
+)
+from .positional import (
+    Position,
+    Track,
+    Sensor,
+    SensorArray,
+)
